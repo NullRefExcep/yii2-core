e7e2b4f8dca11fbf806c296bc30cd4029fcdcafe
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-06-30 19:16:02 +0300

    module installer

--- src/components/ModuleInstaller.php
+++ src/components/ModuleInstaller.php
@@ -5,6 +5,7 @@ namespace nullref\core\components;
 use yii\base\Component;
 use yii\db\Connection;
 use yii\di\Instance;
+use Composer\Installer\PackageEvent;
 
 /**
  *
@@ -23,14 +24,44 @@ class ModuleInstaller extends Component
         $this->db->getSchema()->refresh();
     }
 
-    public function install()
+    public function install(PackageEvent $event)
     {
         //do some stuff
     }
 
-    public function uninstall()
+    public function uninstall(PackageEvent $event)
     {
         //do some stuff
+    }
+
+    /**
+     * Check if table exist
+     * @param $tableName
+     * @return bool
+     */
+    public function tableExist($tableName)
+    {
+        return $this->db->schema->getTableSchema($tableName, true) !== null;
+    }
+
+    /**
+     * Builds and executes a SQL statement for dropping a DB table.
+     * @param string $table the table to be dropped. The name will be properly quoted by the method.
+     */
+    public function dropTable($table)
+    {
+        $this->db->createCommand()->dropTable($table)->execute();
+    }
+
+    /**
+     * Create table by name, columns and options
+     * @param $table
+     * @param $columns
+     * @param null $options
+     */
+    public function createTable($table, $columns, $options = null)
+    {
+        $this->db->createCommand()->createTable($table, $columns, $options)->execute();
     }
 }
 
