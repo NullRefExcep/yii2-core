ebff3e9e9858b6f558dd2f9f00fa2edc91d5450b
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-07-17 18:26:20 +0300

    fix

---
+++ src/behaviors/HasRelation.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author    Dmytro Karpovych
+ * @copyright 2015 NRE
+ */
+
+namespace nullref\core\behaviors;
+
+
+use nullref\core\interfaces\IEntityManageble;
+use nullref\core\traits\EntityManageble;
+use yii\base\Behavior;
+
+abstract class HasRelation extends Behavior implements IEntityManageble
+{
+    use EntityManageble;
+
+    public abstract function getRelationName();
+
+    public abstract function getAttributeName();
+
+    public abstract function getAttributeLabel();
+
+    protected abstract function getRelation();
+
+} 
