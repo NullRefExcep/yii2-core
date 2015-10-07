723ee3b70e8ee65389777397c333240f6cbf423e
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-10-08 00:33:27 +0300

    add beforeInit & afterInit module events

--- src/components/Module.php
+++ src/components/Module.php
@@ -7,14 +7,21 @@
 namespace nullref\core\components;
 
 use Yii;
+use yii\base\Event;
 use yii\base\Module as BaseModule;
 use yii\web\Application as WebApplication;
 
 
 class Module extends BaseModule
 {
+    const EVENT_BEFORE_INIT = 'beforeInit';
+
+    const EVENT_AFTER_INIT = 'afterInit';
+
     public function init()
     {
+        $this->trigger(self::EVENT_BEFORE_INIT);
+
         parent::init();
         /** Add path for views overriding */
         if (Yii::$app instanceof WebApplication) {
@@ -24,5 +31,7 @@ class Module extends BaseModule
             }
             $view->theme->pathMap['@nullref/' . $this->id] = '@app/modules/' . $this->id;
         }
+
+        $this->trigger(self::EVENT_AFTER_INIT);
     }
 } 
