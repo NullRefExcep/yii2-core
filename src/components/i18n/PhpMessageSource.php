77ecc8026d029f042aafab72eab83e0335bd043f
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-11-10 00:53:18 +0200

    add mergeable PhpMessageSource

---
+++ src/components/i18n/PhpMessageSource.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author    Dmytro Karpovych
+ * @copyright 2015 NRE
+ */
+
+namespace nullref\core\components\i18n;
+
+
+use Yii;
+use yii\i18n\PhpMessageSource as BasePhpMessageSource;
+use yii\helpers\ArrayHelper;
+
+class PhpMessageSource extends BasePhpMessageSource
+{
+    protected $_parentMessages = [];
+    protected function loadMessages($category, $language)
+    {
+        $messages = parent::loadMessages($category, $language);
+
+        if ((strpos($category, '*') > 0)){
+            return $messages;
+        }
+        $key = $category . '/' . $language;
+
+        if (!isset($this->_parentMessages[$key])){
+            $parent = Yii::$app->getI18n()->getMessageSource($category.'*');
+            $oldMessages = $parent->loadMessages($category, $language);
+            $this->_parentMessages[$key] = ArrayHelper::merge($messages, $oldMessages);
+        }
+        return $this->_parentMessages[$key];
+    }
+
+}
