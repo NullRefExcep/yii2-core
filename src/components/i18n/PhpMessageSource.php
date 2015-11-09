5dc4788f3af509b29e483f5cbc7c631e1f35d26a
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-11-10 00:56:00 +0200

    fix mergeable PhpMessageSource

--- src/components/i18n/PhpMessageSource.php
+++ src/components/i18n/PhpMessageSource.php
@@ -8,25 +8,26 @@ namespace nullref\core\components\i18n;
 
 
 use Yii;
-use yii\i18n\PhpMessageSource as BasePhpMessageSource;
 use yii\helpers\ArrayHelper;
+use yii\i18n\PhpMessageSource as BasePhpMessageSource;
 
 class PhpMessageSource extends BasePhpMessageSource
 {
     protected $_parentMessages = [];
+
     protected function loadMessages($category, $language)
     {
         $messages = parent::loadMessages($category, $language);
 
-        if ((strpos($category, '*') > 0)){
+        if ((strpos($category, '*') > 0)) {
             return $messages;
         }
         $key = $category . '/' . $language;
 
-        if (!isset($this->_parentMessages[$key])){
-            $parent = Yii::$app->getI18n()->getMessageSource($category.'*');
+        if (!isset($this->_parentMessages[$key])) {
+            $parent = Yii::$app->getI18n()->getMessageSource($category . '*');
             $oldMessages = $parent->loadMessages($category, $language);
-            $this->_parentMessages[$key] = ArrayHelper::merge($messages, $oldMessages);
+            $this->_parentMessages[$key] = ArrayHelper::merge($oldMessages, $messages);
         }
         return $this->_parentMessages[$key];
     }
